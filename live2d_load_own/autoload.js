8bf5affc5c1f20c5be42abf02adcd162473d030d
辰旭 <59323207+Karensky@users.noreply.github.com>
2020-08-16 21:38:29 +0800

    Update autoload.js

--- live2d_load_own/autoload.js
+++ live2d_load_own/autoload.js
@@ -1,6 +1,6 @@
 // 注意：live2d_path 参数应使用绝对路径，记得修改哦
-const live2d_path = "https://你的域名/live2d_load_own/";
-const tips_path = "https://你的域名/live2d_load_own/";
+const live2d_path = "https://cdn.jsdelivr.net/gh/Karensky/cx@1.0.7/live2d_load_own/";
+const tips_path = "https://cdn.jsdelivr.net/gh/Karensky/cx@1.0.7/live2d_load_own/";
 //const live2d_path = "";
 //const live2d_path = "/live2d-widget/";
 
